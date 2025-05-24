c67fc1248a1749dceb2c6f5f2ba86a3c6aaa5e8e
BerniceZTT <zhaotiantian361@126.com>
2025-05-24 18:07:43 +0800

    feat: cros

--- middleware/cors.go
+++ middleware/cors.go
@@ -10,7 +10,7 @@ import (
 // CORS 跨域中间件
 func CORS() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3001", "http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:3001", "http://47.113.230.108:3001", "http://localhost:5173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length", "X-Total-Count"},
